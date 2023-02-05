5db2ca391faa83ab4eed5eacc57f766218d3b3e2
theasrk <124521488+theasrk@users.noreply.github.com>
2023-02-05 18:30:13 -0500

    Update streamlit_app.py
    fixed grammar

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-streamlit.title('My Parents new healthy diner')
+streamlit.title('My Parents\' new healthy diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
