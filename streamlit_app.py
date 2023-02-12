47b6b532b0a08d74642a2eadab1e5146a946b5c0
theasrk <124521488+theasrk@users.noreply.github.com>
2023-02-11 21:49:53 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ streamlit.dataframe(fruits_to_show)
 
 #new section to display fruityvice API response
 #import requests
-streamlit.header("Fruityvice Fruit Advice!")
+
 #create repeatable code block (called a function)
 def get_fruityvice_data(this_fruit_choice):
       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
@@ -34,7 +34,7 @@ def get_fruityvice_data(this_fruit_choice):
       # Outputs the response from fruityvice into a table format
      return fruityvice_normalized
 
-
+streamlit.header("Fruityvice Fruit Advice!")
 try:
     fruit_choice = streamlit.text_input('What fruit would you like information about?')
     if not fruit_choice:
