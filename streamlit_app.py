dc0f4461e759f0e0f7ecd34075705e9405034165
theasrk <124521488+theasrk@users.noreply.github.com>
2023-02-11 21:47:58 -0500

    Update streamlit_app.py
    added a function to get the fruit data

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,18 +25,23 @@ streamlit.dataframe(fruits_to_show)
 #new section to display fruityvice API response
 #import requests
 streamlit.header("Fruityvice Fruit Advice!")
+#create repeatable code block (called a function)
+def get_fruityvice_data(this_fruit_choice):
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+      #streamlit.text(fruityvice_response.json()) # just writes the data to the screen
+      #Converts JSON response to a more readable format
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())  
+      # Outputs the response from fruityvice into a table format
+     return fruityvice_normalized
+
+
 try:
     fruit_choice = streamlit.text_input('What fruit would you like information about?')
     if not fruit_choice:
       streamlit.error("Please select a fruit to get information.")
     else:
-      #streamlit.write('The user entered ', fruit_choice)
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-      #streamlit.text(fruityvice_response.json()) # just writes the data to the screen
-      #Converts JSON response to a more readable format
-      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-      # Outputs the response from fruityvice into a table format
-      streamlit.dataframe(fruityvice_normalized)
+        back_from_function = get_fruityvice_data(fruit_choice)
+        streamlit.dataframe(back_from_function)      
 
 except URLError as e:
    streamlit.error()
