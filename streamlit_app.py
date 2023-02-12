626383ac1437ad633347e2e18a1873aa33800621
theasrk <124521488+theasrk@users.noreply.github.com>
2023-02-11 19:02:28 -0500

    Update streamlit_app.py
    Converted response code to JSON response

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,5 +21,6 @@ streamlit.dataframe(fruits_to_show)
 
 #new section to display fruityvice API response
 import requests
+streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
